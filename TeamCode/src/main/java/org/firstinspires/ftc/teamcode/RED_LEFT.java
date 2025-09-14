074b75a4b5494806ad67a18fde29dbc6c1bd1698
Gearheads13365 <Wesclin.Gearheads@gmail.com>
2025-09-14 15:39:29 -0500

    code cleanup, prep

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RED_LEFT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RED_LEFT.java
@@ -24,60 +24,7 @@ public class RED_LEFT extends LinearOpMode {
         ///////////////////////////////////////////////////////////////////////////////////////////////
         // Everything below this line is what the robot is actually doing during the autonomous mode //
         ///////////////////////////////////////////////////////////////////////////////////////////////
-        robot.resetEncoders();
 
-        int pixelPosition = robot.getIconPosition("RED_LEFT");
-        telemetry.addData("pixelPosition", pixelPosition);
-        telemetry.addData("iconAnalysisMiddle", robot.iconAnalysisMiddle);
-        telemetry.addData("iconAnalysisRight", robot.iconAnalysisRight);
-        telemetry.update();
-
-
-
-        // Pixel in position 6 (right side)
-        if (pixelPosition == 6)
-        {
-            robot.Drive(1250, 0.5, 0);
-            sleep(300);
-            robot.Drive(500, 0.5, -90);
-            sleep(300);
-            //robot.deliverPurplePixel();
-            robot.Drive(-550, 0.5, -90);
-            sleep(300);
-            robot.Drive(-750, 0.5, 0);
-            sleep(300);
-            robot.StrafeDistance(0.5, -370);
-            sleep(300);
-        }
-        // Pixel in position 5 (middle)
-        else if (pixelPosition == 5)
-        {
-            robot.Drive(1630 , 0.5, 0);
-            sleep(300);
-            //robot.deliverPurplePixel();
-            robot.Drive(-1130, 0.5, 0);
-            sleep(300);
-            robot.StrafeDistance(0.5, -420);
-            sleep(300);
-        }
-        // Pixel in position 4 (left side)
-        else //pixelPosition == 4
-        {
-            robot.StrafeDistance(0.5, -420);
-            sleep(300);
-            robot.Drive(1150, 0.5, 0);
-            sleep(300);
-            //robot.deliverPurplePixel();
-            robot.Drive(-650, 0.5, 0);
-            sleep(300);
-        }
-
-        robot.setArmPower(0);
-
-        telemetry.update();
-
-        robot.webcam.stopStreaming();
-      robot.StopDriving();
       //End of Autonomous Program
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -14,11 +14,10 @@ public class Teleop extends LinearOpMode {
         GearHeadRobot robot = new GearHeadRobot(this);
         robot.init();
         robot.imu.resetYaw();
-        robot.resetEncoders();
+
 
         robot.setClawPosition(robot.close);
 
-        robot.webcam.setPipeline(robot.pipelineLTMP);
 
         double liftPower = -1;
         int precisePower = 1;
@@ -30,43 +29,18 @@ public class Teleop extends LinearOpMode {
 
 
             if (gamepad1.dpad_up) {
-                robot.webcam.setPipeline(robot.pipelineLTRP);
-                robot.lastPressed = 1;
+
             }
             if (gamepad1.dpad_down) {
-                robot.webcam.setPipeline(robot.pipelineRTMP);
-                robot.lastPressed = 2;
+
             }
             if (gamepad1.dpad_left) {
-                robot.webcam.setPipeline(robot.pipelineLTMP);
-                robot.lastPressed = 3;
+
             }
             if (gamepad1.dpad_right) {
-                robot.webcam.setPipeline(robot.pipelineRTRP);
-                robot.lastPressed = 4;
+
             }
 
-
-            if (robot.lastPressed == 1)
-            {
-                robot.AvgVal_RED = robot.pipelineLTRP.getAnalysisRED();
-                robot.AvgVal_BLUE = robot.pipelineLTRP.getAnalysisBLUE();
-            }
-            if (robot.lastPressed == 2)
-            {
-                robot.AvgVal_RED = robot.pipelineRTMP.getAnalysisRED();
-                robot.AvgVal_BLUE = robot.pipelineRTMP.getAnalysisBLUE();
-            }
-            if (robot.lastPressed == 3)
-            {
-                robot.AvgVal_RED = robot.pipelineLTMP.getAnalysisRED();
-                robot.AvgVal_BLUE = robot.pipelineLTMP.getAnalysisBLUE();
-            }
-            if (robot.lastPressed == 4)
-            {
-                robot.AvgVal_RED = robot.pipelineRTRP.getAnalysisRED();
-                robot.AvgVal_BLUE = robot.pipelineRTRP.getAnalysisBLUE();
-            }
 
 
             if (gamepad1.right_trigger > 0.5)
@@ -84,93 +58,8 @@ public class Teleop extends LinearOpMode {
                    ((-gamepad1.left_stick_y - gamepad1.left_stick_x) - (gamepad1.right_stick_x))/precisePower,
                    ((-gamepad1.left_stick_y + gamepad1.left_stick_x) - (gamepad1.right_stick_x))/precisePower );
 
-            double heading = robot.getHeading();
-
-            x_position = robot.getAprilTagPosition(2);
-            telemetry.addData("x_position", x_position);
-            telemetry.addData("Heading",heading);
-            telemetry.addData("Encoders", robot.GetMotorEncoders() );
-            //telemetry.addData("Serve1Position", robot.w1.getPosition());
-            //telemetry.addData("Serve2Position", robot.w2.getPosition());
 
 
-            telemetry.addData("LB_Encoder", robot.Get_LB_Encoder());
-            telemetry.addData("LF_Encoder", robot.Get_LF_Encoder());
-            telemetry.addData("RB_Encoder", robot.Get_RB_Encoder());
-            telemetry.addData("RF_Encoder", robot.Get_RF_Encoder());
-
-
-
-            robot.setArmPower(gamepad2.left_stick_y);
-
-            liftPower = -1/precisePower;
-
-            if (gamepad1.right_bumper)
-            {
-                liftState = 1;
-                robot.setLiftPower(1);
-            }
-            else if (gamepad1.left_bumper)
-            {
-                liftState = 0;
-                robot.setLiftPower(liftPower);
-            }
-            else if (liftState == 0)
-            {
-                robot.setLiftPower(0);
-            }
-
-            if (liftState == 1 && robot.getLiftEncoder() > 3469)
-            {
-                    robot.setLiftPower(0);
-                    liftState = 0;
-            }
-            telemetry.addData("Lift_Encoder", robot.Get_Lift_Encoder());
-
-            if (gamepad2.x)
-            {
-                robot.setClawPosition(robot.close);
-            }
-            if (gamepad2.b)
-            {
-                robot.setClawPosition(robot.open);
-            }
-
-            if (gamepad2.right_trigger > 0.15 || gamepad2.left_trigger > 0.15)
-            {
-                robot.setWristPower(gamepad2.left_trigger - gamepad2.right_trigger);
-            }
-            else
-            {
-                robot.setWristPower(0);
-            }
-
-
-
-            if (gamepad1.a)
-            {
-                robot.resetEncoders();
-            }
-            if (gamepad1.b)
-            {
-                robot.imu.resetYaw();
-            }
-
-            if (gamepad1.y)
-            {
-                robot.setPAPower(-1);
-            }
-            else
-            {
-                robot.setPAPower(0);
-            }
-
-            telemetry.addData("armPower", robot.GetArmPower());
-            telemetry.addData("armEncoder", robot.Get_Arm_Encoder());
-            telemetry.addData("servoReportedPos", robot.getClawPosition());
-            telemetry.addData("AvgVal_RED", robot.AvgVal_RED);
-            telemetry.addData("AvgVal_BLUE", robot.AvgVal_BLUE);
-            telemetry.update();
         }
     }
 }
